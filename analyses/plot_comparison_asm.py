e5533755ffc84c815ab3ce2db8f3d17955b98c29
Luca Denti <ldenti@proton.me>
2026-04-30 09:36:31 +0200

    Update comparison heatmaps

--- analyses/plot_comparison_asm.py
+++ analyses/plot_comparison_asm.py
@@ -37,97 +37,95 @@ def main():
     parser.add_argument("--refine", action="store_true")
     parser.add_argument("--diagonal", action="store_true")
     parser.add_argument("--asm", required=True, type=str)
-    # parser.add_argument(
-    #     "--confident",
-    #     action="store_true",
-    # )
+    parser.add_argument(
+        "--confident",
+        action="store_true",
+    )
     parser.add_argument("-o", default="")
     parser.add_argument("WD")
 
     args = parser.parse_args()
 
-    # confident = "conf" if args.confident else "full"
+    confident = "conf" if args.confident else "full"
 
     labels = ["dipcall", "SVIM-asm", "hapdiff"]
     indexes = {"dipcall": 0, "svim-asm": 1, "hapdiff": 2}
 
     INDIRS = [args.WD + "/chm13", args.WD + "/hg38", args.WD + "/hg19"]
 
-    fig, axes = plt.subplots(2, 3, figsize=(9, 6))
+    fig, axes = plt.subplots(1, 3, figsize=(8, 3))
 
-    for row, confident in enumerate(["conf", "full"]):
-        for col, ref in enumerate(["hg19", "hg38", "chm13"]):
-            ax = axes[row][col]
-            ddir = args.WD + "/" + ref
+    for col, ref in enumerate(["hg19", "hg38", "chm13"]):
+        ax = axes[col]
+        ddir = args.WD + "/" + ref
 
-            M = [[0 for _ in labels] for _ in labels]
-            M_annot = [[0 for _ in labels] for _ in labels]
-            for t1, t2 in itertools.product(labels, labels):
-                t1 = t1.lower()
-                t2 = t2.lower()
-                ACC = 0
-                if t1 == t2:
-                    ACC = 1.0
-                    M[indexes[t2]][indexes[t1]] = ACC
-                    M_annot[indexes[t2]][indexes[t1]] = str(ACC)
+        M = [[0 for _ in labels] for _ in labels]
+        M_annot = [[0 for _ in labels] for _ in labels]
+        for t1, t2 in itertools.product(labels, labels):
+            t1 = t1.lower()
+            t2 = t2.lower()
+            ACC = 0
+            if t1 == t2:
+                ACC = 1.0
+                M[indexes[t2]][indexes[t1]] = ACC
+                M_annot[indexes[t2]][indexes[t1]] = str(ACC)
+            else:
+                n = "ga4gh_with_refine.summary" if args.refine else "summary"
+                fn = f"{ddir}/asmcallsets-{args.asm}/comparison-{confident}/{t1}-against-{t2}/{n}.json"
+                if os.path.exists(fn):
+                    P, R, F, TP, FP, FN = parse_summary(fn)
+                    ACC = F  # round(TP / (TP + FP + FN), 2)
                 else:
-                    n = "ga4gh_with_refine.summary" if args.refine else "summary"
-                    fn = f"{ddir}/asmcallsets-{args.asm}/comparison-{confident}/{t1}-against-{t2}/{n}.json"
-                    if os.path.exists(fn):
-                        P, R, F, TP, FP, FN = parse_summary(fn)
-                        ACC = F  # round(TP / (TP + FP + FN), 2)
-                    else:
-                        ACC = 0.0
-                    M[indexes[t2]][indexes[t1]] = ACC
-                    M_annot[indexes[t2]][indexes[t1]] = str(ACC)
+                    ACC = 0.0
+                M[indexes[t2]][indexes[t1]] = ACC
+                M_annot[indexes[t2]][indexes[t1]] = str(ACC)
 
-                    fn = f"{ddir}/asmcallsets-{args.asm}/comparison-{confident}/{t2}-against-{t1}/{n}.json"
-                    if os.path.exists(fn):
-                        P, R, F, TP, FP, FN = parse_summary(fn)
-                        ACC = F  # round(TP / (TP + FP + FN), 2)
-                    else:
-                        ACC = 0.0
-                    M[indexes[t1]][indexes[t2]] = ACC
-                    M_annot[indexes[t1]][indexes[t2]] = str(ACC)
+                fn = f"{ddir}/asmcallsets-{args.asm}/comparison-{confident}/{t2}-against-{t1}/{n}.json"
+                if os.path.exists(fn):
+                    P, R, F, TP, FP, FN = parse_summary(fn)
+                    ACC = F  # round(TP / (TP + FP + FN), 2)
+                else:
+                    ACC = 0.0
+                M[indexes[t1]][indexes[t2]] = ACC
+                M_annot[indexes[t1]][indexes[t2]] = str(ACC)
 
-            if args.diagonal:
-                # Force diagonal
-                M[1][0] = 0
-                M_annot[1][0] = ""
-                M[2][0] = 0
-                M_annot[2][0] = ""
-                M[2][1] = 0
-                M_annot[2][1] = ""
+        if args.diagonal:
+            # Force diagonal
+            M[1][0] = 0
+            M_annot[1][0] = ""
+            M[2][0] = 0
+            M_annot[2][0] = ""
+            M[2][1] = 0
+            M_annot[2][1] = ""
 
-            sns.heatmap(
-                M,
-                # square=True,
-                annot=M_annot,
-                fmt="",
-                cmap=CMAP,
-                xticklabels=labels if row == 1 else False,
-                yticklabels=labels if col == 0 else False,
-                cbar=False,
-                vmin=0.3,
-                vmax=1.0,
-                ax=ax,
-            )
-            if row == 1:
-                ax.tick_params(axis="x", labelrotation=0)
-            if col == 0:
-                ax.tick_params(axis="y", labelrotation=90)
+        sns.heatmap(
+            M,
+            square=True,
+            annot=M_annot,
+            fmt="",
+            cmap=CMAP,
+            xticklabels=labels,
+            yticklabels=labels if col == 0 else False,
+            cbar=False,
+            vmin=0.3,
+            vmax=1.0,
+            ax=ax,
+        )
+        ax.tick_params(axis="x", labelrotation=0)
+        if col == 0:
+            ax.tick_params(axis="y", labelrotation=90)
     titles = ["GRCh37", "GRCh38", "T2T-CHM13"]
     for col, title in enumerate(titles):
-        axes[0][col].set_title(title)
+        axes[col].set_title(title)
 
     # # axes[1].set_xlabel("How much of this...")
     # # axes[0].set_ylabel("...matches with this")
-    axes[0][0].set_ylabel("Confident regions\n\nTruth set")
-    axes[1][0].set_ylabel("Full genome\n\nTruth set")
+    # axes[0][0].set_ylabel("Confident regions\n\nTruth set")
+    # axes[1][0].set_ylabel("Full genome\n\nTruth set")
 
-    axes[1][0].set_xlabel("Call set")
-    axes[1][1].set_xlabel("Call set")
-    axes[1][2].set_xlabel("Call set")
+    # axes[1][0].set_xlabel("Call set")
+    # axes[1][1].set_xlabel("Call set")
+    # axes[1][2].set_xlabel("Call set")
     # fig.supylabel("Truth set")
 
     plt.tight_layout()
